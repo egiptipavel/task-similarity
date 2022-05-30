80988c9b90e64343288164178b09ca74e618080f
egiptipavel <kinqstone585@gmail.com>
2022-05-30 15:32:37 +0300

    Add README

--- main.py
+++ main.py
@@ -7,4 +7,4 @@ app = FastAPI()
 app.include_router(router)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", port=8000, host="0.0.0.0", reload=True)
+    uvicorn.run("main:app", port=8000, host="0.0.0.0", reload=False)
